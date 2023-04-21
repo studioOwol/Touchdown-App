54dcb8385c72bd2fb4182ccb84e5569233991d08
studioOwol <from.owol@gmail.com>
2023-04-21 17:54:14 +0900

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Nahyun on 2023/04/21.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
