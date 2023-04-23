b1af941da54022b4933ba1c6f940182ff446978f
studioOwol <from.owol@gmail.com>
2023-04-23 18:11:08 +0900

    Page Tab View

---
+++ Touchdown/Model/PlayerModel.swift
@@ -0,0 +1,13 @@
+//
+//  PlayerModel.swift
+//  Touchdown
+//
+//  Created by Nahyun on 2023/04/23.
+//
+
+import Foundation
+
+struct Player: Codable, Identifiable {
+    let id: Int
+    let image: String
+}
--- Touchdown/Utility/Constant.swift
+++ Touchdown/Utility/Constant.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 // Data
+let players: [Player] = Bundle.main.decode("player.json")
 // Color
 let colorBackground: Color = Color("ColorBackground")
 let colorGray: Color = Color(UIColor.systemGray4)
