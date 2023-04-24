092005a5c05b35bf1af091998633f8075170f60b
studioOwol <from.owol@gmail.com>
2023-04-24 15:58:09 +0900

    Product Detail View - Part 2

---
+++ Touchdown/View/Detail/AddToCartDetailView.swift
@@ -0,0 +1,38 @@
+//
+//  AddToCartDetailView.swift
+//  Touchdown
+//
+//  Created by Nahyun on 2023/04/24.
+//
+
+import SwiftUI
+
+struct AddToCartDetailView: View {
+    var body: some View {
+        Button(action: {}, label: {
+            Spacer()
+            Text("Add to cart".uppercased())
+                .font(.system(.title2, design: .rounded))
+                .fontWeight(.bold)
+                .foregroundColor(.white)
+            Spacer()
+        })//: Button
+        .padding(15)
+        .background(
+            Color(
+                red: sampleProduct.red,
+                green: sampleProduct.green,
+                blue: sampleProduct.blue
+            )
+        )
+        .clipShape(Capsule())
+    }
+}
+
+struct AddToCartDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddToCartDetailView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
